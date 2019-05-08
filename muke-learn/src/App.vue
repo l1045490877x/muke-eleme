80b473529ee62736e0e09540f5ce90b3752c6aec
Brain Lu <l1045490877x@163.com>
2019-05-08 14:42:04 +0800

    add two projects

---
+++ muke-learn/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Vheader :seller="seller"></Vheader>
+  </div>
+</template>
+
+<script>
+import Vheader from 'components/v-header/v-header'
+import { getSeller } from 'api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    Vheader
+  },
+  created () {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller () {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
