3c107419e22876de2fb3c9feb48fbc3f0d63ffd7
Brain Lu <l1045490877x@163.com>
2019-05-11 21:53:58 +0800

    完善1

--- muke-learn/src/App.vue
+++ muke-learn/src/App.vue
@@ -11,12 +11,43 @@
 import Vheader from 'components/v-header/v-header'
 import { getSeller } from 'api'
 import tab from 'components/tab/tab'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
 
 export default {
   name: 'app',
   data () {
     return {
       seller: {}
+    }
+  },
+  computed: {
+    tabs() {
+      return [
+        {
+          label:'商品',
+          component:goods,
+          data:{
+            seller:this.seller
+          }
+        },
+        {
+          label:'评论',
+          component:ratings,
+          data:{
+            seller:this.seller
+          }
+        },
+        {
+          label:'商家',
+          component:Seller,
+          data:{
+            seller:this.seller
+          }
+        },
+      ]
+
     }
   },
   components: {
