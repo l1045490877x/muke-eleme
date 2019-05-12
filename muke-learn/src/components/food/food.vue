a2dc67f0739b50606b0232de712348807e24cf11
Brain Lu <l1045490877x@163.com>
2019-05-12 22:32:48 +0800

    food.vue样式不完整

--- muke-learn/src/components/food/food.vue
+++ muke-learn/src/components/food/food.vue
@@ -44,10 +44,184 @@
                             :desc = "desc"
                             :ratings = "ratings">
                         </rating-select>
-                        
+                        <div class="rating-wrapper">
+                            <ul v-show="computedRatings && computedRatings.length">
+                                <li 
+                                    v-for="(rating,index) in computedRatings"
+                                    class="rating-item border-bottom-1px"
+                                    :key="index"
+                                >
+                                    <div class="user">
+                                        <span class="name">{{rating.username}}</span>
+                                        <img width="12" height="12" :src="ratings.avatar" >
+                                    </div>
+                                    <div class="time">{{format(rating.rateTime)}}</div>
+                                    <p class="text">
+                                        <span :class="{'icon-thum_up':rating.rateType===0,'icon-thumb_down':rating.ratType===1}"></span>{{rating.text}}
+                                    </p>
+                                </li>
+                            </ul>
+                            <div class="no-rating" v-show="!computedRatings || !computedRatings.length">暂无评价</div>
+                        </div>
                     </div>
                 </div>
             </cube-scroll>
         </div>
     </transition>
 </template>
+
+<script>
+import moment from 'moment'
+import CartControl from 'components/cart-control/cart-control'
+import RatingSelect from 'components/rating-select/rating-select'
+import Split from 'components/split/split'
+import ragingMixin from 'components/mixins/rating'
+import popupMixin from 'components/mixins/popup'
+
+const EVENT_SHOW = 'show'
+const EVENT_ADD = 'add'
+const EVENT_LEAVE = 'leave'
+
+export default {
+    name:'food',
+    mixins: [ratingMixin, popupMixin],
+    props: {
+        food: {
+            type: Object
+        }
+    },
+    data() {
+        return {
+            desc: {
+                all: '全部',
+                positive: '推荐',
+                negative: '吐槽'
+            }
+        }
+    },
+    computed: {
+        ratings() {
+            return this.food.ratings
+        }
+    },
+    created() {
+        this.$on(EVENT_SHOW, () => {
+            this.$nextTick(() => {
+                this.$refs.scroll.refresh()
+            })
+        })
+    },
+    methods: {
+        afterLeave() {
+            this.$emit(EVENT_LEAVE)
+        },
+        addFirst(event) {
+            this.$set(this.food, 'count', 1)
+            this.$emit(EVENT_ADD, target)
+        },
+        format(time) {
+            return moment(time).format('YYYY-MM-DD hh:mm')
+        }
+    },
+    components:{
+        CartControl,
+        RatingSelect,
+        Split
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin.styl"
+
+    .food
+        position fixed
+        left 0
+        top 0
+        bottom 48px
+        z-index 30 
+        width 100%
+        background $color-white
+        &.move-enter-active,&.cube-action-sheet-move-leave-active
+            transition all 0.3s linear
+        &.move-enter,&move-leave-active
+            transform translate3d(100%, 0, 0)
+        .image-header
+            position relative
+            width 100%
+            height 0
+            padding-top 100%
+            img 
+                position absolute 
+                top 0
+                left 0
+                width 100%
+                height 100%
+            .back
+                position absolute 
+                top 10px
+                left 0
+                .icon-arrow_lift
+                    display block
+                    padding 10px
+                    font-size $fontsize-large-xx
+                    color $color-white
+        .content
+            position relative
+            padding 18px
+            .title
+                line-height 14px
+                margin-bottom 8px
+                font-size $fontsize-medium
+                font-weight 700
+                color $color-dark-grey
+            .detail 
+                margin-bottom 18px
+                line-height 10px
+                height 10px
+                .sell-count,.rating
+                    font-size $fontsize-small-s
+                    color $color-light-grey
+                .sell-count
+                    margin-right 12px
+            .price 
+                line-height 24px
+                font-weight 700
+                .now
+                    margin-right 8px
+                    font-size 14px
+                    color $color-red
+                .old
+                    text-decoration line-height
+                    font-size $fontsize-small-s
+                    color $color-light-grey
+            .cart-control-wrapper
+                position relative
+                right 18px
+                bottom 18px
+                z-index 10
+                height 24px
+                line-height 24px
+                padding 0 12px
+                box-sizing border-box
+                border-radius 12px
+                font-size $fontsize-small-s
+                color $color-white
+                background $color-blue
+                opacity 1
+                &.fade-enter-active,&.fade-leave-active
+                    transition all 0.3s
+                &.fade-enter,&.fade-leave-active
+                    opacity 0
+                    z-index -1
+        .info
+            padding 18px
+            .title
+                line-height 14px
+                margin-bottom 6px
+                font-size $fontsize-medium
+                color $color-dark-grey 
+            .text
+</style>
+
