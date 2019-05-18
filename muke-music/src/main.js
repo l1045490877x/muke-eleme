527b769c4d0dc136e1387e40acff9426e77fcea2
Brain Lu <l1045490877x@163.com>
2019-05-18 22:59:38 +0800

    添加 common文件下的文件夹及内容，主要为font和stylus文件，暂时运行报错

--- muke-music/src/main.js
+++ muke-music/src/main.js
@@ -2,14 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 
-Vue.config.productionTip = false
-
+// Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  // router,
+  // components: { App },
+  // template: '<App/>'
+  render: h => h(App)
 })
